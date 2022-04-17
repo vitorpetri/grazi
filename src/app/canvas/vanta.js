70a9f537e72c25abc2770876dcd5397113e9c5f6
vitor <laabenne@gmail.com>
2022-04-17 11:02:17 -0300

    Updates.

---
+++ src/app/canvas/vanta.js
@@ -0,0 +1,14 @@
+VANTA.NET({
+  el: "#vanta-canvas",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x78742,
+  backgroundColor: 0xcfcfcf,
+  points: 15.00,
+  maxDistance: 10.00
+})
